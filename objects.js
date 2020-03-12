c1befee177c7c3d93337a09d450e9dc2d0759706
curious-metric-5246 <richardjharvey1@gmail.com>
2020-03-12 18:30:03 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ playlist.kaleo = ['Way down we go'];
 playlist.m83 = ['ElectroPunk'];
 
 function updatePlaylist(playlist, artist, song){
-  playlist.artist = [[song]];
+  playlist[artist] = song;
   return playlist;
 }
